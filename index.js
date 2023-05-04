bd23fbb43aa8ea5af68566dd9fccee55d0f41b95
Arthur Seus <arthur.seus.correa@gmail.com>
2023-05-04 15:09:11 -0300

    working on it

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use('/', bodyParser.urlencoded({ extended: false }));
 
 app.use(cors())
 app.use(express.static('public'))
@@ -10,7 +22,30 @@ app.get('/', (req, res) => {
 });
 
 
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
 
+  const newUser = new User({
+    username: username
+  })
+
+  newUser.save((err, saved) => {
+    if(err){
+      console.error(err)
+    } else {
+      console.log(`saved: ${saved}`)
+      User.findOne({ username: username }, (err, user) => {
+        if(err){
+          console.error(err)
+        } else {
+          console.log(user)
+          res.json({ "username": user['username'], "_id": user['_id']})
+        }
+      })
+    }
+  })
+
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
